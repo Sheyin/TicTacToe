161e37166475120c40d30f15f0525612735dfdb4
Sheyin <Sheyin@users.noreply.github.com>
2014-12-05 11:14:36 -0600

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class TicTacToe {
 
 	public static void main (String[] args) {
-		char[] grid;
+		char[] grid = {' ',' ',' ', ' ',' ', ' ',' ', ' ',' '};
 		int moveCount = 0;
-		boolean victory = false;
+		int victory = -1;
 		boolean playerturn = CoinFlip();
 		do {
 			PrintGrid(grid);
@@ -18,13 +18,13 @@ class TicTacToe {
 				playerturn = false;
 				}
 			victory = VictoryCheck(grid);
-			movecount++;
-			} while((movecount < 10) || (!victory));
-		DeclareVictory(grid);
+			moveCount++;
+			} while((moveCount < 10) && (victory < 0));
+		DeclareVictory(victory);
 		}
 		
-	boolean CoinFlip() {
-		int randnum = (int)(Math.rand());
+	public static boolean CoinFlip() {
+		int randnum = (int)(Math.random());
 		if (randnum < 0.45) {
 			return true;
 			}
@@ -33,7 +33,7 @@ class TicTacToe {
 			}
 		}
 		
-	void PrintGrid(char[] grid) {
+	public static void PrintGrid(char[] grid) {
 		//char[9] fgrid = ConvGrid(grid);
 		System.out.println("/n " + grid[0] + " | " + grid[1] + " | " + grid[2]);
 		System.out.println("/n " + grid[3] + " | " + grid[4] + " | " + grid[5]);
@@ -41,38 +41,22 @@ class TicTacToe {
 		return;
 		}
 	
-	/* char[] ConvGrid(int[] grid) {
-		char [] fgrid;
-		for (int i=0, i<10, i++) {
-			if (grid[i] == -1) {
-				fgrid[i] = 'O';
-				}
-			if (grid[i] == -2) {
-				fgrid[i] == 'X';
-				}
-			else {	// no mark / empty
-				fgrid[i] == " ";
-				}
-			}
-		return fgrid;
-		} */
-	
-	char[] AddPoint(char[] grid, int spot, boolean playerTurn) {
-		if (playerturn)
+	public static char[] AddPoint(char[] grid, int spot, boolean playerTurn) {
+		if (playerTurn)
 			grid[spot] = 'O';
 		else	// comp turn
 			grid[spot] = 'X';
 		return grid;
 		}
 		
-	boolean LegalMove(char[] grid, int spot) {
+	public static boolean LegalMove(char[] grid, int spot) {
 		if ((grid[spot] == ' ') && (spot >= 0) && (spot <=10))
 			return true;
 		else
 			return false;
 		}
 		
-	char[] PlayerMove(char[] grid) {
+	public static char[] PlayerMove(char[] grid) {
 		int move = 0;
 		boolean isLegal = false;
 		do {
@@ -86,60 +70,104 @@ class TicTacToe {
 			else {
 				System.out.println("This spot is taken.  Try another spot.");
 					}
-		} while(!isLegal)
+		} while(!isLegal);
+		return grid;	//is this needed?
 	}
 
 		
-	int PlayerInput(){
+	public static int PlayerInput(){
 		Scanner user_input = new Scanner(System.in);
-		String playerInput = user_input.next();
-		boolean validInput = InputCheck(user_input.charAt(0));
+		String playerInput = user_input.nextLine();
+		char toTest = playerInput.charAt(0);
+		boolean validInput = InputCheck(toTest);
 		do {
 			if (validInput) {
-				int spot = parseInt(user_input[0]-1);
+				int spot = Character.getNumericValue(toTest);
+				//int spot = parseInt(user_input[0]-1);
 				return spot;
 				}
 			else { 	// invalid input
 				System.out.println("Invalid input.  Please pick a square by choosing a number.");
-				String playerInput = user_input.next();
+				playerInput = user_input.nextLine();
 				}
-			} while (!validInput)
+			} while (!validInput);
+			return -1; //is this needed?
 		}
 		
-	boolean InputCheck (String toCheck) {	// don't forget to subtract 1 when checking
+	public static boolean InputCheck (int toCheck) {	// don't forget to subtract 1 when checking
+	/* fix me
 		try {	// this blocks test if it is an integer at all
-			Integer.parseInt(toCheck); 
+			//Integer.parseInt(String); 
 			} 
 		catch(NumberFormatException e) { 
 			return false; 
 			}
-		int checkMe = (toCheck.charAt(0) - 1);	
+		int checkMe = (toCheck.charAt(0) - 1);	*/
 		return true;
 		}
 
-	char[] CompMove (char[] grid) {		// decides optimal move - victory, defend, some move in same row, then random move
-		int [] results = MoveCheck(grid);
-		int spot = CheckResults(results,2)
-		
-		if (VictoryMoveCheck(grid)) {
-			grid = VictoryMove(grid);
+	public static char[] CompMove (char[] grid) {		// decides optimal move - victory, defend, some move in same row, then random move
+		int [] summary;
+		boolean isLegal = false;
+		summary = MoveCheck(grid);
+		int spot = FindSpot(summary);
+		if (spot == -1) {
+			do {
+				spot = (int)(Math.random() * 10);
+				isLegal = LegalMove(grid,spot);
+				} while(!isLegal);
 			}
-		if (OneXRow(grid)) {		// try to do one in same row
-			grid = AddtoNextX(grid);
+		else {	// need to turn spot (a row) into a location
+			int [] rownumbers = IDRow(spot);	// this returns the array locations in grid of that row
+			spot = FindBlank(grid, rownumbers);
 			}
-		else {						//random move - ex. first move
-			grid = RandomMove(grid);
-			}
+		grid = AddPoint(grid,spot,false);
 		return grid;
 		}
 		
-	int [] MoveCheck (char[] grid) {		// summarize each of the rows
-		int[] testspots = [0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6];
-		int[] sortedresults;
+	public static int FindBlank(char[] grid, int[] row) {
+		char toTest;
+		
+		for (int i=0;i<3;i++){
+			toTest = grid[row[i]];
+			if (toTest == ' '){
+				return i; }
+		}
+		return -1;	// is this needed?
+	}
+
+	public static int VictoryCheck (char [] grid) {
+		int[] summary = MoveCheck(grid);
+		for(int i=0; i<16; i++){
+			if (summary[i] == 3) {
+				return i;
+				}
+			}
+			// no one won yet
+				return -1;
+	}
+	
+	public static void DeclareVictory(int winner) {
+		if (winner > 7) {		//player wins
+			System.out.println("You win!");
+			}
+		if ((winner >= 0) && (winner < 8)) {	// computer wins
+			System.out.println("You lose!");
+			}
+		else {	//draw (?)
+			System.out.println("It's a draw!");
+			}
+	}
+		
+	public static int[] MoveCheck (char[] grid) {		// summarize each of the rows
+		int[] testspots = {0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6};
+		int[] sortedresults = {0};
 		int XCount = 0;
 		int OCount = 0;
 		int nullCount = 0;
-		for (int i = 0, i < 25, i++){
+		int[] unsorted = {0};
+		int rowcount = 0;
+		for (int i = 0; i < 25; i++){
 			if (grid[i] == 'X'){
 				XCount++; }
 			if (grid[i] == 'O'){
@@ -149,11 +177,11 @@ class TicTacToe {
 			rowcount++;
 			
 			if ((i+1)%3 == 0){
-				rawresult[i-2] = XCount;
-				rawresult[i-1] = OCount;
-				rawresult[i] = nullCount;
+				unsorted[i-2] = XCount;
+				unsorted[i-1] = OCount;
+				unsorted[i] = nullCount;
 				i = i+3;
-				sortedresults = SortResults (rawresults);
+				sortedresults = SortResults (unsorted);
 				
 				//reset counters
 				XCount = 0;
@@ -164,8 +192,9 @@ class TicTacToe {
 		return sortedresults;
 		}
 		
-	int[] SortResults(int[] unsorted) {
-		int[] sorted;
+	public static int[] SortResults(int[] unsorted) {
+		int[] sorted = {0};
+		int i = 0;
 		do {
 			sorted[i] = unsorted[i];
 			sorted[i+8] = unsorted[i+1];
@@ -175,7 +204,7 @@ class TicTacToe {
 		return sorted;
 	}
 	
-	int[] TestSpots(){		//this just returns the pattern of rows used on the grid to match up spots.
+	public int[] TestSpots(){		//this just returns the pattern of rows used on the grid to match up spots.
 		//rownum 1: int[] row1 = [0, 1, 2];
 		//rownum 2: int[] row2 = [3, 4, 5];
 		//rownum 3: int[] row3 = [6, 7, 8];
@@ -184,28 +213,26 @@ class TicTacToe {
 		//rownum 6: int[] col3 = [2, 5, 8];
 		//rownum 7: int[] diag1 = [0, 4, 8];
 		//rownum 8: int[] diag2 = [2, 4, 6];
-		int[] testspots = [0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6];
+		int[] testspots = {0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6};
 		return testspots;
 	}
 	
-	int FindSpot(int[] summary) {	//this is searching for particular parameters.  i<8 means XX_, i<16 means OO_.
-		for (int i=0, i<8, i++) {	// this is just checking for two X's and a blank
+	public static int FindSpot(int[] summary) {	//this is searching for particular parameters.  i<8 means XX_, i<16 means OO_.
+		for (int i=0; i<8; i++) {	// this is just checking for two X's and a blank
 			if ((summary[i] == 2) && (summary[i+8] == 1)) {
 				return i; }		//this is the row where there is a legal move - need to find the blank spot
-			else
 				// nothing... just let it increment i
-			}
-		for (int i=8, i<16, i++) {	// this is now searching for two O's and a blank.  Most likely to be used.
+		}
+		for (int i=8; i<16; i++) {	// this is now searching for two O's and a blank.  Most likely to be used.
 			if ((summary[i] == 2) && (summary[i+8] == 1)) {
 				return i; }		//this is the row where there is a legal move - need to find the blank spot
-			else
 				// nothing... just let it increment i
-			}
+		}
 		return -1;		//no spots found matching this pattern.
 	}
 	
-	int[] IDRow(int rownumber) {		// this identifies the row that should be moved to and returns the spots.
-		//rownum 1: int[] row1 = [0, 1, 2];
+	public static int[] IDRow(int rownumber) {		// this identifies the row and returns the spots.
+		//rownum 1: int[] row1 = [0, 1, 2];			// method will know which spots to look in grid
 		//rownum 2: int[] row2 = [3, 4, 5];
 		//rownum 3: int[] row3 = [6, 7, 8];
 		//rownum 4: int[] col1 = [0, 3, 6];
@@ -213,82 +240,65 @@ class TicTacToe {
 		//rownum 6: int[] col3 = [2, 5, 8];
 		//rownum 7: int[] diag1 = [0, 4, 8];
 		//rownum 8: int[] diag2 = [2, 4, 6];
-		int[] thisrow;
+		
 		switch (rownumber) {
-			case 1:
-				thisrow = [0, 1, 2];
-			case 2:
-				thisrow = [3, 4, 5];
-			case 3:
-				thisrow = [6, 7, 8];
-			case 4:
-				thisrow = [0, 3, 6];
-			case 5:
-				thisrow = [1, 4, 7];
-			case 6:
-				thisrow = [2, 5, 8];
-			case 7:
-				thisrow = [0, 4, 8];
-			case 8:
-				thisrow = [2, 4, 6];
+			case 1: {
+				int[] thisrow = {0, 1, 2};
+				return thisrow;	
+				}
+			case 2: {
+				int[] thisrow = {3, 4, 5};
+				return thisrow;	
+				}
+			case 3: {
+				int[] thisrow = {6, 7, 8};
+				return thisrow;	
+				}
+			case 4: {
+				int[] thisrow = {0, 3, 6};
+				return thisrow;	
+				}
+			case 5: {
+				int[] thisrow = {1, 4, 7};
+				return thisrow;	
+				}
+			case 6: {
+				int[] thisrow = {2, 5, 8};
+				return thisrow;	
+				}
+			case 7: {
+				int[] thisrow = {0, 4, 8};
+				return thisrow;	
+				}
+			case 8: {
+				int[] thisrow = {2, 4, 6};
+				return thisrow;	
+				}
+			default: {
+				int[] thisrow = {0, 0, 0};
+				return thisrow;	
+				}
 		}
-		return thisrow;		// method will know which spots to look in grid
 	}
 
-	int FindBlankSpot(int[] spots, char[] grid) {
-		for(int i = 0, i < 3, i++) {
+	public int FindBlankSpot(int[] spots, char[] grid) {
+		for(int i = 0; i < 3; i++) {
 			if (grid[spots[i]] == ' ') {
 				return spots[i]; }
-			else
-				//redo for loop
+			//else redo for loop
 		}
+		return -1;
 	}
 	
-	int[] RandomMove (int[] grid) {
-		int doThis = 0;
+	public char[] RandomMove (char[] grid) {
+		int move = 0;
 		boolean isLegal = false;
 		do {
-			doThis = (Math.rand()*10);
-			isLegal = LegalMove(grid, doThis); 
+			move = (int)(Math.random()*10);
+			isLegal = LegalMove(grid, move); 
 				} while (!isLegal);
 		grid = AddPoint(grid, move, false);
 		return grid;
 	}
 	
-		// left the following deprecated methods until I'm sure I won't need them anymore.
-	
-	int CheckResults (int[] results, int pattern) {		//searches results array for specified pattern
-		int spot = 0;
-		return spot;
-	}
-	
-	int[] VictoryMove (int[] grid) {
-		isLegal = LegalMove(grid, move);
-		grid = AddPoint(grid, move, false);
-	}
-	
-	boolean DefendMoveCheck (int[] grid) {
-		return true; // temp
-	}
-	
-	int[] DefendMove (int[] grid) {
-		isLegal = LegalMove(grid, move);
-		grid = AddPoint(grid, move, false);
-	}
-	
-	boolean OneXRow (int[] grid) {
-		return true; // temp
-	}
-	
-	int[] AddtoNextX (int[] grid) {
-		isLegal = LegalMove(grid, move);
-		grid = AddPoint(grid, move, false);
-	}
-	
-	int[] RandomMove (int[] grid) {
-		isLegal = LegalMove(grid, move);
-		grid = AddPoint(grid, move, false);
-	}
-	
-	int RowChecker (int[] grid                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 }
